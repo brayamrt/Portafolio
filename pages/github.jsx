d7a51b7e0fa256b083db94b55075d4168c0e23b1
brayamrt <bdricardo71@gmail.com>
2023-10-06 10:32:22 -0500

    Actualizacion Pages GitHub

--- pages/github.jsx
+++ pages/github.jsx
@@ -4,7 +4,7 @@ import RepoCard from '../components/RepoCard';
 import styles from '../styles/GithubPage.module.css';
 
 const GithubPage = ({ repos, user }) => {
-  // console.log(repos);
+ 
   const theme = {
     level0: '#161B22',
     level1: '#0e4429',
@@ -28,20 +28,20 @@ const GithubPage = ({ repos, user }) => {
             <h3 className={styles.username}>{user.login}</h3>
           </div>
           <div>
-            <h3>{user.public_repos} repos</h3>
+            <h3>{user.public_repos} Repositorios</h3>
           </div>
           <div>
-            <h3>{user.followers} followers</h3>
+            <h3>{user.followers} Seguidores</h3>
           </div>
         </div>
       </a>
-      <div> <center><h3>My Most Popular Repositories on Github</h3></center></div>
+      <div> <center><h3>Mis Repositorios mas Populares en Github</h3></center></div>
       <div className={styles.container}>
         {repos.map((repo) => (
           <RepoCard key={repo.id} repo={repo} />
         ))}
       </div>
-      <div><center><h3>My Github Calendar</h3></center></div>
+      <div><center><h3>Calendario de Contribuciones en GitHub</h3></center></div>
       <br />
       <center>
         <div className={styles.contributions}>
