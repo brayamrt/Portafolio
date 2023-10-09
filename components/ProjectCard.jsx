f864a87e093d2c7ac8c2bc358554e1cd842f41ef
brayamrt <bdricardo71@gmail.com>
2023-10-09 16:42:17 -0500

    Actualizar componente ProjectCard

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/ProjectCard.module.css';
 const ProjectCard = ({ project }) => {
   return (
     <div className={styles.card}>
-      <Image src={project.image} height={300} width={600} alt={project.name} />
+      <Image src={project.image} height={160} width={316} alt={project.name} />
       <div className={styles.content}>
         <h3>{project.name}</h3>
         <p>{project.description}</p>
